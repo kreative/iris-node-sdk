cb8080bf82e24e70af1847b0a2c3bbf35f4ea02f
Armaan Gupta <guppythegod@tcff.co>
2020-05-08 15:35:56 -0500

    Rewrote Iris class

--- index.js
+++ index.js
@@ -1,103 +1,74 @@
 const axios = require("axios");
 
-const IRIS_URL = process.env.IRIS_URL || "https://iris.kreative.im";
-
 class Iris {
-    
-    constructor(aidn, serviceKey) {
+
+    constructor(aidn, irisUrl) {
         this.aidn = parseInt(aidn);
-        this.serviceKey = serviceKey;
+        this.url = irisUrl + "/log";
     }
 
     debug(message, meta, tags) {
-        axios.post(`${IRIS_URL}/log`, {
+        axios.post(this.url, {
             message,
             tags,
             meta,
             level: 'debug',
             aidn: this.aidn
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'portal_calling_aidn': this.aidn,
-                'portal_service_key': this.serviceKey
-            }
         })
         .catch(error => {return error})
         .then(response => {return response});
     };
 
     info(message, meta, tags) {
-        axios.post(`${IRIS_URL}/log`, {
+        axios.post(this.url, {
             message,
             tags,
             meta,
             level: 'info',
             aidn: this.aidn
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'portal_calling_aidn': this.aidn,
-                'portal_service_key': this.serviceKey
-            }
         })
         .catch(error => {return error})
         .then(response => {return response});
     };
 
     error(message, meta, tags) {
-        axios.post(`${IRIS_URL}/log`, {
+        axios.post(this.url, {
             message,
             tags,
             meta,
             level: 'error',
             aidn: this.aidn
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'portal_calling_aidn': this.aidn,
-                'portal_service_key': this.serviceKey
-            }
         })
-        .catch(error => {return error})
+        .catch(error => {
+            console.log(error);
+            return error
+        })
         .then(response => {return response});
     };
 
     warn(message, meta, tags) {
-        axios.post(`${IRIS_URL}/log`, {
+        axios.post(this.url, {
             message,
             tags,
             meta,
             level: 'warn',
             aidn: this.aidn
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'portal_calling_aidn': this.aidn,
-                'portal_service_key': this.serviceKey
-            }
         })
         .catch(error => {return error})
         .then(response => {return response});
     };
 
     critical(message, meta, tags) {
-        axios.post(`${IRIS_URL}/log`, {
+        axios.post(this.url, {
             message,
             tags,
             meta,
             level: 'critical',
             aidn: this.aidn
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'portal_calling_aidn': this.aidn,
-                'portal_service_key': this.serviceKey
-            }
         })
         .catch(error => {return error})
         .then(response => {return response});
     };
-};
+}
 
 module.exports = Iris;
--- test1.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const Iris = require("./index");
-
-const iris = new Iris(process.env.AIDN, process.env.SK);
-
-iris.error('test 5555', {code:2}, ["#test"]);
